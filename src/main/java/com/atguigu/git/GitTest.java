baeb0b14f475ae0254df9bea41dfb1ab332a8145
BGoose <3253954317@qq.com>
2022-10-26 21:06:55 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @Description
+ * @data Created in 2022-10-26 21:04
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
